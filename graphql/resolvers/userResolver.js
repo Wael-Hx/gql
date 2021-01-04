62199b4cff383d52f69e2fec7dd53724af9aa5b0
Wael-Hx <wael.lhb@gmail.com>
2021-01-04 13:56:41 +0100

    login , register, logout to Users model

--- graphql/resolvers/userResolver.js
+++ graphql/resolvers/userResolver.js
@@ -1,6 +1,5 @@
 const { UserInputError } = require("apollo-server");
 const Users = require("../../models/users");
-const User = require("../../db/models/User");
 const { validateInput, validateLogin } = require("../../utils/validateInput");
 require("dotenv").config();
 
@@ -25,65 +24,23 @@ module.exports = {
       { credentials: { username, email, password } },
       { req }
     ) => {
-      try {
-        const { valid, message } = validateInput({ username, email, password });
-        if (!valid) {
-          return new UserInputError(message);
-        }
-        const user = await User.findOne({ $or: [{ email }, { username }] });
-        if (user) {
-          return new UserInputError("user already exists");
-        }
-        const newUser = new User({ username, email, password });
-        const res = await newUser.save();
-        req.session.userId = res.id;
-
-        return { id: res.id, ...res._doc };
-      } catch (err) {
-        console.error(err);
-        return err;
+      const { valid, message } = validateInput({ username, email, password });
+      if (!valid) {
+        return new UserInputError(message);
       }
+      const user = await Users.register({ username, email, password }, req);
+      return user;
     },
     login: async (_, { credentials: { email, password } }, { req }) => {
-      try {
-        const { valid, message } = validateLogin({ email, password });
-        if (!valid) {
-          return new UserInputError(message);
-        }
-        const user = await User.findOne({ email });
-        if (!user) {
-          return new UserInputError(
-            "user not found , make sure you type the correct credentials"
-          );
-        } else {
-          const isMatch = await user.comparePassword(password);
-          if (isMatch) {
-            req.session.userId = user.id;
-
-            return user;
-          } else {
-            return new UserInputError(
-              "make sure you type the correct credentials"
-            );
-          }
-        }
-      } catch (err) {
-        console.error(err);
-        return err;
+      const { valid, message } = validateLogin({ email, password });
+      if (!valid) {
+        return new UserInputError(message);
       }
+      const user = await Users.login({ email, password }, req);
+      return user;
     },
     logout: async (_, __, { req, res }) => {
-      return new Promise((resolve) => {
-        req.session.destroy((err) => {
-          if (err) {
-            console.error(err);
-            resolve("something wrong happened");
-            return;
-          }
-          res.clearCookie("UAT");
-          resolve("ok");
-        });
-      });
+      return Users.logout(req, res);
     },
   },
 };
--- models/users.js
+++ models/users.js
@@ -1,6 +1,64 @@
 const User = require("../db/models/User");
-const { ApolloError, AuthenticationError } = require("apollo-server");
+const {
+  ApolloError,
+  AuthenticationError,
+  UserInputError,
+} = require("apollo-server");
 const { checkAuth } = require("../utils/auth");
+
+const register = async ({ username, email, password }, req) => {
+  try {
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+    if (user) {
+      return new UserInputError("user already exists");
+    }
+    const newUser = new User({ username, email, password });
+    const res = await newUser.save();
+    req.session.userId = res.id;
+
+    return { id: res.id, ...res._doc };
+  } catch (err) {
+    console.error(err);
+    return err.message;
+  }
+};
+
+const login = async ({ email, password }, req) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return new UserInputError(
+        "user not found , make sure you type the correct credentials"
+      );
+    } else {
+      const isMatch = await user.comparePassword(password);
+      if (isMatch) {
+        req.session.userId = user.id;
+
+        return user;
+      } else {
+        return new UserInputError("make sure you type the correct credentials");
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    return err.message;
+  }
+};
+
+const logout = (req, res) => {
+  return new Promise((resolve) => {
+    req.session.destroy((err) => {
+      if (err) {
+        console.error(err);
+        resolve("something wrong happened");
+        return;
+      }
+      res.clearCookie("UAT");
+      resolve("ok");
+    });
+  });
+};
 
 const getUser = async (userId) => {
   if (!userId) {
@@ -42,4 +100,7 @@ module.exports = Users = {
   getUser,
   getUserById,
   getAllUsers,
+  register,
+  login,
+  logout,
 };
